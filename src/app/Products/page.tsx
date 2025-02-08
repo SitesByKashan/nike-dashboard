5551ab5ee5757f037d1c757250e840e713981b5f
Kashan Malik <sitesbykashan@gmail.com>
2025-02-08 23:05:45 +0500

    .

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -70,35 +70,7 @@ const page = async () => {
         </TableBody>
       </Table>
     </div>
-       {/* <Table>
-       <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-    <TableRow>
-      <TableHead className="w-[100px]">Invoice</TableHead>
-      <TableHead>Status</TableHead>
-      <TableHead>Method</TableHead>
-      <TableHead className="text-right">Amount</TableHead>
-    </TableRow>
-  </TableHeader>
-
-      {result.map((item:any)=>{
-        return(
-         
-  <TableBody>
-    <TableRow>
-      <TableCell className="font-medium">{item.name}</TableCell>
-      <TableCell>{item.price}</TableCell>
-      <TableCell>{item.originalPrice}</TableCell>
-      <TableCell className="text-right">{item.category}</TableCell>
-    </TableRow>
-  </TableBody>
-
-        )
-      })}
-</Table> */}
-      {/* <Header/> */}
-
-    </div>
+ 
   )
 }
 
