363c028efbc08f08693eb5a02ab101e24b2804d6
Kashan Malik <sitesbykashan@gmail.com>
2025-02-08 23:15:24 +0500

    .

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -1,31 +1,9 @@
-import { client } from '@/lib/client';
+
 import React from 'react'
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import Image from 'next/image';
-import { urlFor } from '@/lib/image';
+
 
 const page = async () => {
-  const result = await client.fetch(`
-    *[_type == "food"]{
-      _id,
-      name,
-      category,
-      price,
-      originalPrice,
-      image,
-      description,
-      available,
-      tags
-    }
-  `);
+  
   return (
     <div>
     
